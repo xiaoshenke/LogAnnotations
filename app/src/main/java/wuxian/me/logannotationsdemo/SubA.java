5d896efc1e4495bdcf1cb9c49bdbf969b49ce197
xiaoshenke <1205914137@qq.com>
2016-12-11 15:46:12 +0800

    upgrade LittleParser

--- app/src/main/java/wuxian/me/logannotationsdemo/SubA.java
+++ app/src/main/java/wuxian/me/logannotationsdemo/SubA.java
@@ -1,8 +1,6 @@
 package wuxian.me.logannotationsdemo;
 
 import android.util.Log;
-
-
 import wuxian.me.logannotations.LOG;
 import wuxian.me.logannotations.LogAll;
 import wuxian.me.logannotations.NoLog;
@@ -15,13 +13,14 @@ import wuxian.me.logannotationsdemo.sub.SubMainActivity;
 
 public class SubA<T extends MainActivity & IWhatever & IWhat> extends A {
 
-
     private void call(String name, String xing) {
+
+        Log.i("SubA", "in func call ,powered by LOG Annotation");
 
     }
 
-
     public void call(String name) {
         super.call(name);
+        Log.i("SubA", "in func call ,powered by LOG Annotation");
     }
 }
